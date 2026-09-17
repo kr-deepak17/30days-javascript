f81654f89037871384122b05163afe820f7c3192
kr-deepak17 <kr10deepak19@gmail.com>
2026-09-18 02:03:22 +0530

    we have learnt error handling

---
+++ errorHandling/index.js
@@ -0,0 +1,21 @@
+// // let x= 5;
+// try{
+//     console.log("try block start here")
+//     console.log(x);
+//     console.log("try block ends here")
+// }
+// catch(err){
+//     // console.log("your error is here:",err)
+//     throw new Error("mein hu custom error bhai tune x ko declare nhi kra hai bhai")
+
+// }
+// finally{
+//     console.log("mein hu finally mein to chalunga ho")
+// }
+
+let errorCode=100;
+if(errorCode==100){
+    throw new Error("Invalid json")
+}
+
+
